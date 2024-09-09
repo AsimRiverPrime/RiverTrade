d38b297a114bdd83141949cb5dec18f558ebf449
AsimRiverPrime <apple_dev@riverprime.com>
2024-09-09 13:40:26 +0400

    - add screens for completeProfile

---
+++ RiverPrime/Tabbar Controllers/Profile/KYC/Compelet_verification/CompleteVerificationProfileScreen1.swift
@@ -0,0 +1,24 @@
+//
+//  CompleteVerificationProfileScreen1.swift
+//  RiverPrime
+//
+//  Created by abrar ul haq on 08/09/2024.
+//
+
+import UIKit
+
+class CompleteVerificationProfileScreen1: BottomSheetController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //MARK: - This commented code is use where we want to call this class.
+        //MARK: - Asim bhai ye mene calling k lie kr dia he 1 class k lie, ap ne jahan bhi ye call krni hogi ye commented code use kr lain.
+        /*
+        let vc = Utilities.shared.getViewController(identifier: .completeVerificationProfileScreen1, storyboardType: .bottomSheetPopups) as! CompleteVerificationProfileScreen1
+        PresentModalController.instance.presentBottomSheet(self, sizeOfSheet: .large, VC: vc)
+        */
+        
+    }
+    
+}
